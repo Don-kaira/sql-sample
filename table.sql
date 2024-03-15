a8fef9b8e7de5e5d5c19b67adf3961005ce2d73d
Don Don kaira <57275581+Don-kaira@users.noreply.github.com>
2024-03-15 17:42:57 +0200

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,7 +1,6 @@
- 
- create table DEPTS 
-      deptno   num,
+ create table DEPTS (
+      deptno   number,
 	  name     varchar2(40) not null,
 	  location varchar2(40),
-	  constraint pk_depts primary key(deptno
+	  constraint pk_depts primary key(deptno)
 );
