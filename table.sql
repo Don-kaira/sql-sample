dd3a902b10510db59fd7083f7f08e78544242745
Don Don kaira <57275581+Don-kaira@users.noreply.github.com>
2025-09-10 09:35:11 +0200

    Update table.sql

--- table.sql
+++ table.sql
@@ -4,3 +4,20 @@
 	  location varchar2(40),
 	  constraint pk_depts primary key(deptno)
 );
+
+create table EMPLOYEES (  
+  empno             number,  
+  name              varchar2(50) not null,  
+  surname           varchar2(50),
+  job               varchar2(50),  
+  mobile            number varchar(15),
+  email             varchar(30),
+  manager           number,  
+  hiredate          date,  
+  salary            number(7,2),  
+  commission        number(7,2),  
+  deptno           number,  
+  constraint pk_employees primary key (empno),  
+  constraint fk_employees_deptno foreign key (deptno) 
+      references DEPARTMENTS (deptno)  
+);
